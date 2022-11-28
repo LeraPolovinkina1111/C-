f07fbc17b693a73358925e75f83242b2367c6be6
LeraPolovinkina111 <leo.plv@bk.ru>
2022-11-28 19:15:10 +0500

    задача2

---
+++ zadacha2/Program.cs
@@ -0,0 +1,39 @@
+﻿int a1=2;
+int b1=3;
+int c1=7;
+int max1=0;
+if (a1>b1) max1=a1;
+else max1=b1;
+if (c1>max1) max1=c1;
+else 
+{
+    Console.WriteLine(max1);
+}
+Console.WriteLine(max1);
+
+
+int a2=44;
+int b2=5;
+int c2=78;
+int max2=0;
+if (a2>b2) max2=a2;
+else max2=b2;
+if (c2>max2) max2=c2;
+else 
+{
+    Console.WriteLine(max2);
+}
+Console.WriteLine(max2);
+
+
+int a3=22;
+int b3=3;
+int c3=9;
+int max3=0;
+if (a3>b3) max3=a3;
+else max3=b3;
+if (c3>max3) max3=c3;
+else 
+{
+    Console.WriteLine(max3);
+}
