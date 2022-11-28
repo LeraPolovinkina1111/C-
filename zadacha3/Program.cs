87a92a866f8f3f771806d675cd68d17c26acfaa6
LeraPolovinkina111 <leo.plv@bk.ru>
2022-11-28 19:37:31 +0500

    задача3

--- zadacha3/Program.cs
+++ zadacha3/Program.cs
@@ -1,33 +1,13 @@
-﻿int a=4;
-if (a % 2 == 1)
-{
-    Console.WriteLine( a + " является: НЕЧЁТНЫМ");
-}
+﻿Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+if (num % 2 == 0)
+
+    Console.WriteLine("Введенное число четное");
+
 else
-{
-   Console.WriteLine(  a + " является: ЧЁТНЫМ");
-}
+    Console.WriteLine("Введенное число нечетное");
+          
+          
 
-
-int b= -3;
-if (b % 2 == 1)
-{
-    Console.WriteLine(  b + " является: НЕЧЁТНЫМ");
-}
-else
-{
-   Console.WriteLine( b + " является: ЧЁТНЫМ");
-}
-
-
-
-int c=7;
-if (c % 2 == 1)
-{
-    Console.WriteLine(+ c + " является: НЕЧЁТНЫМ");
-}
-else
-{
-   Console.WriteLine( + c + " является: ЧЁТНЫМ");
-}
-           
+ 
