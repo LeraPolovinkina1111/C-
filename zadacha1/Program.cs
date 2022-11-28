5d15a3443fef14e74ecdead19301b3ac4441b99e
LeraPolovinkina111 <leo.plv@bk.ru>
2022-11-28 17:03:45 +0500

    задача1

---
+++ zadacha1/Program.cs
@@ -0,0 +1,20 @@
+﻿int a1=5;
+int b1=7;
+int max1=0;
+if (a1>b1) max1=a1;
+else max1=b1;
+Console.WriteLine(max1);
+
+int a2=2;
+int b2=10;
+int max2=0;
+if (a2>b2) max2=a2;
+else max2=b2;
+Console.WriteLine(max2);
+
+int a3=-9;
+int b3=-3;
+int max3=0;
+if (a3>b3) max3=a3;
+else max3=b3;
+Console.WriteLine(max3);
