a4dd830aafabfaba399ab47026425faa68b6406f
LeraPolovinkina111 <leo.plv@bk.ru>
2022-11-28 20:17:43 +0500

    задача4

---
+++ zadacha4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int z = 1;
+int N = int.Parse(Console.ReadLine());
+while (z<=N) 
+{
+    if (z % 2 == 0)
+            
+        Console.Write( z );
+
+    z++;
+}
+
