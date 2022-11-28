fab43e1edcf1a376bb79fb75f08b9898586e239f
LeraPolovinkina111 <leo.plv@bk.ru>
2022-11-28 19:28:14 +0500

    задача3

---
+++ zadacha3/Program.cs
@@ -0,0 +1,33 @@
+﻿int a=4;
+if (a % 2 == 1)
+{
+    Console.WriteLine( a + " является: НЕЧЁТНЫМ");
+}
+else
+{
+   Console.WriteLine(  a + " является: ЧЁТНЫМ");
+}
+
+
+int b= -3;
+if (b % 2 == 1)
+{
+    Console.WriteLine(  b + " является: НЕЧЁТНЫМ");
+}
+else
+{
+   Console.WriteLine( b + " является: ЧЁТНЫМ");
+}
+
+
+
+int c=7;
+if (c % 2 == 1)
+{
+    Console.WriteLine(+ c + " является: НЕЧЁТНЫМ");
+}
+else
+{
+   Console.WriteLine( + c + " является: ЧЁТНЫМ");
+}
+           
